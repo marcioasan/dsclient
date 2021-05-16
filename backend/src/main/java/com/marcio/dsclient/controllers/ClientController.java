3574da8b8a794cda63d4eae88ae480c6e9f2a48a
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-05-16 16:25:13 -0300

    Initial commit

---
+++ backend/src/main/java/com/marcio/dsclient/controllers/ClientController.java
@@ -0,0 +1,5 @@
+package com.marcio.dsclient.controllers;
+
+public class ClientController {
+
+}
---
+++ backend/src/test/java/com/marcio/dsclient/DsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
