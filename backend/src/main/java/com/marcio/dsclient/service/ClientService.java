29d64844070ef15250d48e32ba4edc796ebdc1d4
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-05-16 18:42:27 -0300

    Atualizando e deletando clientes

--- backend/src/main/java/com/marcio/dsclient/service/ClientService.java
+++ backend/src/main/java/com/marcio/dsclient/service/ClientService.java
@@ -3,7 +3,11 @@ package com.marcio.dsclient.service;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -41,6 +45,27 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);					
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
