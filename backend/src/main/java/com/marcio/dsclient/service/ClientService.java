895a0dab9af7eac768790ebd2f6240d8d6b12faf
Marcio Antonio dos Santos <marcioasan@gmail.com>
2021-05-16 18:22:21 -0300

    Buscando cliente - findById

--- backend/src/main/java/com/marcio/dsclient/service/ClientService.java
+++ backend/src/main/java/com/marcio/dsclient/service/ClientService.java
@@ -1,13 +1,17 @@
 package com.marcio.dsclient.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marcio.dsclient.dto.ClientDTO;
 import com.marcio.dsclient.entities.Client;
 import com.marcio.dsclient.repositories.ClientRepository;
+import com.marcio.dsclient.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,9 +19,17 @@ public class ClientService {
 	@Autowired
 	ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(client -> new ClientDTO(client));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 
 }
